0c13b9134f8939c38f74264a6d61756ba7ee4b93
ss1234 <ss450555@gmail.com>
2020-12-01 11:04:22 +0000

    Formatter

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
+    id("org.jmailen.kotlinter") version "3.2.0"
 }
 
 group = "me.siddharthsharma"
--- src/main/kotlin/AoC Day 1.kt
+++ src/main/kotlin/AoC Day 1.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun findPair(input: Array<Int>, target: Int): Int {
     val storedNums: BooleanArray = BooleanArray(target) { false }
     for (n in input) {
-        if(target >= n && storedNums[target - n]) {
+        if (target >= n && storedNums[target - n]) {
             return (target - n) * n
         }
         storedNums[n] = true
@@ -12,13 +12,13 @@ fun findPair(input: Array<Int>, target: Int): Int {
 }
 
 fun findTriplet(input: Array<Int>, target: Int): Int {
-    for(i in 2 until input.size) {
-        if(input[i] > target) {
+    for (i in 2 until input.size) {
+        if (input[i] > target) {
             return 0
         }
-        if(input[i] != input[i - 1]) {
+        if (input[i] != input[i - 1]) {
             val pairSum = findPair(input.sliceArray(0 until i), target - input[i])
-            if(pairSum != 0) return pairSum * input[i]
+            if (pairSum != 0) return pairSum * input[i]
         }
     }
     return 0
