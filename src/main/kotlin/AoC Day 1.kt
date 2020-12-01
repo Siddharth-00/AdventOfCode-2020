c956f367921aa304556cee2ea5392137f3add40d
ss1234 <ss450555@gmail.com>
2020-12-01 10:59:38 +0000

    AoC Day 1 Part 2
    Part 2 for Day 1: Find three numbers in a list that sum to 2020. I did this by iterating through the list and doing findPair on the preceeding list to see if a pair of numbers add up to 2020 - n, where n is the current number, exists.

--- src/main/kotlin/AoC Day 1.kt
+++ src/main/kotlin/AoC Day 1.kt
@@ -1,22 +1,38 @@
 import java.io.File
 
-fun findPair(input: Array<Int>): Int {
-    val storedNums: BooleanArray = BooleanArray(2020) { false }
+fun findPair(input: Array<Int>, target: Int): Int {
+    val storedNums: BooleanArray = BooleanArray(target) { false }
     for (n in input) {
-        if(storedNums[2020 - n]) {
-            return (2020 - n) * n
+        if(target >= n && storedNums[target - n]) {
+            return (target - n) * n
         }
         storedNums[n] = true
     }
     return 0
 }
 
+fun findTriplet(input: Array<Int>, target: Int): Int {
+    for(i in 2 until input.size) {
+        if(input[i] > target) {
+            return 0
+        }
+        if(input[i] != input[i - 1]) {
+            val pairSum = findPair(input.sliceArray(0 until i), target - input[i])
+            if(pairSum != 0) return pairSum * input[i]
+        }
+    }
+    return 0
+}
+
 fun processFile(): Array<Int> {
-    val file = File("/Users/siddharthsharma/Documents/GitHub/AdventOfCode/src/main/resources/input1.txt")
+    val file = File("/Users/siddharthsharma/Documents/GitHub/AdventOfCode-2020/src/main/resources/input1.txt")
     return file.readLines().map { it.toInt() }.toTypedArray()
 }
 
 fun main() {
+    val startTime = System.nanoTime()
     val input: Array<Int> = processFile()
-    println(findPair(input))
+    println(findTriplet(input, 2020))
+    val endTime = System.nanoTime() - startTime
+    println("Time: ${endTime * 0.000001}ms")
 }
